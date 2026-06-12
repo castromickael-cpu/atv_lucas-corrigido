ba42bbe06539437089aff530a69129d210b25e67
castromickael-cpu <castro.mickael@escola.pr.gov.br>
2026-06-12 09:13:45 -0300

    registro de valores

---
+++ registros.sql
@@ -0,0 +1,10 @@
+insert into perfil_usuario(id_login, nome, visualizações, seguidores) values
+(1,'kael', 33, 28);
+
+
+insert into posts(id_usuario, comentario) values
+(1,'bom dia');
+
+insert into likes(id_postagem) values(1);
+
+insert into respostas(id_post,conteudo) values (1,'só pra vc mn');
